2558fa840561649139c4d46dfd8d5e7694db3fdc
ivan <ivan.vanir@gmail.com>
2020-12-08 13:17:31 +0200

    1. Reworked the static files

--- BairBudalite_test1/settings.py
+++ BairBudalite_test1/settings.py
@@ -130,10 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = join(BASE_DIR, 'bairbudalite_static')
+
 STATICFILES_DIRS = (
     join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = join(BASE_DIR, 'bairbudalite_static')
 
 MEDIA_ROOT = join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
