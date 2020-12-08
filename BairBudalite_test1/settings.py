b8dabada2b4feec83a15bd933f23b768b7e70c57
ivan <ivan.vanir@gmail.com>
2020-12-08 13:53:34 +0200

    1. Reworked the static files

--- BairBudalite_test1/settings.py
+++ BairBudalite_test1/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = join(BASE_DIR, 'bairbudalite_static')
+STATIC_ROOT = join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
     join(BASE_DIR, 'static'),
