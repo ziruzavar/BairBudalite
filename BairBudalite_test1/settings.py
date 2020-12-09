37b064874729ca0acbc8e063c22ee2fe216b6c9f
ivan <ivan.vanir@gmail.com>
2020-12-09 20:48:51 +0200

    1. Reworked the static files

--- BairBudalite_test1/settings.py
+++ BairBudalite_test1/settings.py
@@ -54,7 +54,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,7 +137,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
     join(BASE_DIR, 'static'),
 )
